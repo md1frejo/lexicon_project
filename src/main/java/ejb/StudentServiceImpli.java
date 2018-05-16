b7d371a56434f6a12a94640a0dc44c4f2dd5b1e1
Halimove01 <halim.dakir@gmail.com>
2018-05-16 15:52:20 +0200

    Add files via upload

---
+++ src/main/java/ejb/StudentServiceImpli.java
@@ -0,0 +1,35 @@
+package ejb;
+
+import domain.StudentDomain;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class StudentServiceImpli implements StudentService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public void addStudent(StudentDomain student) {
+
+    }
+
+    @Override
+    public void updateStudent(StudentDomain student) {
+
+    }
+
+    @Override
+    public StudentDomain getStudent(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<StudentDomain> getStudents() {
+        return null;
+    }
+}
