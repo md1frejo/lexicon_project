39d58a4d09425d776d842a7725773596cd47f6da
Jonas Fredriksson <jonas@Jonass-MacBook-Pro.local>
2018-05-17 11:58:22 +0200

    several changes

---
+++ src/main/java/jsf/CourseBean.java
@@ -0,0 +1,115 @@
+package jsf;
+
+/* Created by Halim */
+
+public class StudentBean {
+    private Long id;
+    private String pnumber;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String dateOfBirth;
+    private String telNumber;
+    private String adress;
+    private String postzip;
+    private String city;
+    private String country;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPnumber() {
+        return pnumber;
+    }
+
+    public void setPnumber(String pnumber) {
+        this.pnumber = pnumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPostzip() {
+        return postzip;
+    }
+
+    public void setPostzip(String postzip) {
+        this.postzip = postzip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
