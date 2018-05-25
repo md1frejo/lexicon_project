8b2cfb4ff508eb302ec780674fdce0eed1c554ca
Halimove01 <halim.dakir@gmail.com>
2018-05-25 10:11:26 +0200

    Add files via upload

--- src/main/java/jsf/LoginBean.java
+++ src/main/java/jsf/LoginBean.java
@@ -1,3 +1,52 @@
+<<<<<<< HEAD
+package jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@ManagedBean
+@RequestScoped
+public class LoginBean {
+    private String email;
+    private String password;
+    private boolean rememberMe;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+
+    public String login(){
+        return "studentspace";
+    }
+
+    public String register(){
+        return "registerStudents";
+    }
+}
+=======
 package jsf;
 
 import javax.faces.bean.ManagedBean;
@@ -45,3 +94,4 @@ public class LoginBean {
         return "register";
     }
 }
+>>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
--- src/main/java/jsf/StudentsBean.java
+++ src/main/java/jsf/StudentsBean.java
@@ -1,3 +1,204 @@
+<<<<<<< HEAD
+package jsf;
+
+import domain.StudentDomain;
+import domain.UsersDomain;
+import ejb.StudentService;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.*;
+import java.util.Date;
+
+@ManagedBean
+@RequestScoped
+public class StudentsBean {
+    private Long id;
+    @NotNull(message = "*")
+    @Pattern(regexp = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", message = "Exemple@email.com")
+    private String email;
+    @NotNull (message = "*")
+    @Size(min = 8, max = 15, message = "8 =< size =< 15")
+    private String password;
+    @NotNull (message = "*")
+    @Pattern(regexp = "[0-9]{6}-[0-9]{4}", message = "YYYYMMDD-NNNN")
+    private String pnumber;
+    /*@NotNull (message = "*")
+    @Pattern(regexp = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", message = "Exemple@email.com")
+    private String email;*/
+    @NotNull (message = "*")
+    private String firstname;
+    @NotNull (message = "*")
+    private String lastname;
+    /*@NotNull (message = "*")
+    private String password;*/
+    @Past
+    @NotNull (message = "*")
+    //@Pattern(regexp = "[0-9]{4}-[0-9]{2}-[0-9]{2}", message = "YYYY-MM-DD")
+    private Date dateOfBirth;
+    @NotNull (message = "*")
+    @Pattern(regexp = "[0-9]{4}-[0-9]{6}", message = "0700-000000")
+    private String telNumber;
+    @NotNull (message = "*")
+    private String adress;
+    @NotNull (message = "*")
+    @Pattern(regexp = "[0-9]{5}", message = "Ex 12345")
+    private String postzip;
+    @NotNull (message = "*")
+    private String city;
+
+    @AssertTrue (message = "*")
+    private boolean agree;
+
+    @EJB
+    StudentService studentService;
+    public void addUser(){
+        if (getId()==null)
+        studentService.addStudent(new StudentDomain(getPnumber(),getFirstname(),getLastname(),getDateOfBirth(),getTelNumber(),getAdress(),getPostzip(),getCity()), new UsersDomain(getEmail(), getPassword()));
+
+        else
+            studentService.updateStudent(new StudentDomain(getId(), getPnumber(),  getFirstname(), getLastname(), getDateOfBirth(), getTelNumber(), getAdress(), getPostzip(), getCity()), new UsersDomain(getId(), getEmail(),getPassword()));
+
+        setId(null);
+        setEmail("");
+        setPassword("");
+        setPnumber("");
+        setFirstname("");
+        setLastname("");
+        setDateOfBirth(null);
+        setTelNumber("");
+        setAdress("");
+        setPostzip("");
+        setCity("");
+        setAgree(false);
+    }
+
+    public String editUser(Long id){
+        StudentDomain studentDomain = studentService.getStudent(id);
+        setId(studentDomain.getId());
+        setPnumber(studentDomain.getPnumber());
+        setFirstname(studentDomain.getFirstname());
+        setLastname(studentDomain.getLastname());
+        setDateOfBirth(studentDomain.getDateOfBirth());
+        setTelNumber(studentDomain.getTelNumber());
+        setAdress(studentDomain.getAdress());
+        setPostzip(studentDomain.getPostzip());
+        setCity(studentDomain.getPostzip());
+
+
+        return "user";
+    }
+
+    public String removeUser(Long id){
+        studentService.removeStudent(id);
+        return "user";
+    }
+
+    public String getSubmitButtonLabel(){
+        if (id==null)
+            return "Register";
+        else
+            return "Update";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPnumber() {
+        return pnumber;
+    }
+
+    public void setPnumber(String pnumber) {
+        this.pnumber = pnumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPostzip() {
+        return postzip;
+    }
+
+    public void setPostzip(String postzip) {
+        this.postzip = postzip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean isAgree() {
+        return agree;
+    }
+
+    public void setAgree(boolean agree) {
+        this.agree = agree;
+    }
+}
+=======
 package jsf;
 import domain.StudentDomain;
 import ejb.StudentService;
@@ -200,3 +401,4 @@ public class StudentsBean {
         this.agree = agree;
     }
 }
+>>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
