69fd339d58c7240f8b3b4d4b8a922109ea8e4864
Halimove01 <halim.dakir@gmail.com>
2018-06-04 14:25:35 +0200

    Add files via upload

--- src/main/java/domain/TeacherDomain.java
+++ src/main/java/domain/TeacherDomain.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package domain;
 
 import java.util.Date;
@@ -111,56 +110,3 @@ public class TeacherDomain {
         this.city = city;
     }
 }
-=======
-package domain;
-
-/**
- * Created by Mr. Xiao on 2018-05-17.
- */
-
-public class TeacherDomain {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email_adress;
-
-    public TeacherDomain(int id, String firstName, String lastName, String email_adress) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email_adress = email_adress;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    
-    public String getemail_adress() {
-        return email_adress;
-    }
-
-    public void setemail_adress(String email_adress) {
-        this.email_adress = email_adress;
-    }
-}
->>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
