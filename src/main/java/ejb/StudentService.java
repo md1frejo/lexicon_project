0d5c1e66673ca1e06f859471cde38115d0a1c1e4
Halimove01 <halim.dakir@gmail.com>
2018-06-04 14:24:39 +0200

    Add files via upload

--- src/main/java/ejb/StudentService.java
+++ src/main/java/ejb/StudentService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ejb;
 
 import domain.StudentDomain;
@@ -13,26 +12,8 @@ public interface StudentService {
   void addStudent(StudentDomain student, UsersDomain users);
   void  updateStudent(StudentDomain student, UsersDomain users);
     StudentDomain getStudent(Long id);
+    UsersDomain getUsers(Long id);
     void removeStudent(Long id);
-    List<StudentDomain> getStudents();
-  public List<StudentDomain> getStudentsPNumberContain(String filter) ;
+    List<UsersDomain> getUsers();
+  public List<StudentDomain> getStudentsPNumberContain(String filter);
 }
-=======
-package ejb;
-
-import domain.StudentDomain;
-
-import javax.ejb.Local;
-import java.util.List;
-
-@Local
-public interface StudentService {
-
-  void addStudent(StudentDomain student);
-  void  updateStudent(StudentDomain student);
-    StudentDomain getStudent(Long id);
-    void removeStudent(Long id);
-    List<StudentDomain> getStudents();
-  public List<StudentDomain> getStudentsPNumberContain(String filter) ;
-}
->>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
--- src/main/java/ejb/StudentServiceImpli.java
+++ src/main/java/ejb/StudentServiceImpli.java
@@ -1,30 +1,31 @@
-<<<<<<< HEAD
 package ejb;
 
 import domain.StudentDomain;
 import domain.UsersDomain;
 import jpa.Student;
-import jpa.Users;
+import jpa.Login;
 
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Stateless
 public class StudentServiceImpli implements StudentService {
 
-    @PersistenceContext
+    @PersistenceContext(unitName = "Persistence-unit")
     EntityManager entityManager;
 
     @Override
     public void addStudent(StudentDomain student, UsersDomain users) {
         Student student1 = new Student(student.getPnumber(), student.getFirstname(), student.getLastname(),  student.getDateOfBirth(), student.getTelNumber(), student.getAdress(), student.getPostzip(), student.getCity());
         entityManager.persist(student1);
-        Users users1 = new Users(users.getEmail(), users.getPassword(),student1);
-        entityManager.persist(users1);
+        Login login1 = new Login(users.getEmail(), users.getPassword(),student1);
+        entityManager.persist(login1);
 
 
     }
@@ -32,6 +33,9 @@ public class StudentServiceImpli implements StudentService {
     @Override
     public void updateStudent(StudentDomain student, UsersDomain users) {
         Student student1 = entityManager.find(Student.class, student.getId());
+        Login login1 = entityManager.find(Login.class, users.getId());
+        login1.setEmail(users.getEmail());
+        login1.setPassword(users.getPassword());
         student1.setPnumber(student.getPnumber());
         student1.setFirstname(student.getFirstname());
         student1.setLastname(student.getLastname());
@@ -41,31 +45,51 @@ public class StudentServiceImpli implements StudentService {
         student1.setPostzip(student.getPostzip());
         student1.setCity(student.getCity());
         entityManager.merge(student1);
+        entityManager.merge(login1);
 
-        Users users1 = entityManager.find(Users.class, users.getId());
-        users1.setEmail(users.getEmail());
-        users1.setPassword(users.getPassword());
-        entityManager.merge(users1);
 
     }
 
     @Override
     public StudentDomain getStudent(Long id) {
-       Student student1 = entityManager.find(Student.class, id);
+      Student student1= entityManager.find(Student.class, id);
        return new StudentDomain(student1.getId(), student1.getPnumber(), student1.getFirstname(), student1.getLastname(), student1.getDateOfBirth(),student1.getTelNumber(),student1.getAdress(),student1.getPostzip(), student1.getCity());
     }
 
     @Override
-    public void removeStudent(Long id) {
-        Student student1 = entityManager.find(Student.class, id);
-        entityManager.remove(student1);
+    public UsersDomain getUsers(Long id) {
+        Login login1 = entityManager.find(Login.class, id);
+        return new UsersDomain(login1.getId(), login1.getEmail(), login1.getPassword());
     }
 
     @Override
-    public List<StudentDomain> getStudents() {
-
-        return null;
+    public void removeStudent(Long id) {
+        Login login1 = entityManager.find(Login.class, id);
+        entityManager.remove(login1);
+        /*Student student1 = entityManager.find(Student.class, id);
+        entityManager.remove(student1);*/
     }
+
+    @Override
+    public List<UsersDomain> getUsers() {
+        Query query = entityManager.createQuery("select a from Login a where a.email= :mail and a.password = :pass", Login.class);
+        query.setParameter("mail", "halim@gmail.com");
+        query.setParameter("pass", "123");
+
+        List<Login> personRecords = query.getResultList();
+        System.out.println("user Size " + personRecords.size()) ;
+
+        List<Login> l = entityManager.createNamedQuery("selectAllS").getResultList();
+
+        List<UsersDomain> myList = new ArrayList<>();
+        for (Login u:l){
+            myList.add(new UsersDomain(u.getId(),u.getEmail(),u.getPassword()));
+        }
+        return l.stream().
+                map(u->new UsersDomain(u.getId(),u.getEmail(),u.getPassword())).
+                collect(Collectors.toList());
+    }
+
 
     @Override
     public List<StudentDomain> getStudentsPNumberContain(String filter) {
@@ -76,69 +100,3 @@ public class StudentServiceImpli implements StudentService {
                 .collect(Collectors.toList());
     }
 }
-=======
-package ejb;
-
-import domain.StudentDomain;
-import jpa.Student;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-
-@Stateless
-public class StudentServiceImpli implements StudentService {
-
-    @PersistenceContext
-    EntityManager entityManager;
-
-    @Override
-    public void addStudent(StudentDomain student) {
-        Student student1 = new Student(student.getPnumber(), student.getEmail(), student.getFirstname(), student.getLastname(), student.getPassword(), student.getDateOfBirth(), student.getTelNumber(), student.getAdress(), student.getPostzip(), student.getCity(), student.getCountry());
-        entityManager.persist(student1);
-
-
-    }
-
-    @Override
-    public void updateStudent(StudentDomain student) {
-        Student student1 = entityManager.find(Student.class, student.getId());
-        student1.setPnumber(student.getPnumber());
-        student1.setEmail(student.getEmail());
-        student1.setFirstname(student.getFirstname());
-        student1.setLastname(student.getLastname());
-        student1.setPassword(student.getPassword());
-        student1.setDateOfBirth(student.getDateOfBirth());
-        student1.setTelNumber(student.getTelNumber());
-        student1.setAdress(student.getAdress());
-        student1.setPostzip(student.getPostzip());
-        student1.setCity(student.getCity());
-        student1.setCountry(student.getCountry());
-
-        entityManager.merge(student1);
-
-    }
-
-    @Override
-    public StudentDomain getStudent(Long id) {
-        return null;
-    }
-
-    @Override
-    public void removeStudent(Long id) {
-        Student student1 = entityManager.find(Student.class, id);
-        entityManager.remove(student1);
-    }
-
-    @Override
-    public List<StudentDomain> getStudents() {
-        return null;
-    }
-
-    @Override
-    public List<StudentDomain> getStudentsPNumberContain(String filter) {
-        return null;
-    }
-}
->>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
