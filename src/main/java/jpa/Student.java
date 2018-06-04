bf8ff085a914e0144991aef1d0a668ba009e6cf2
Halimove01 <halim.dakir@gmail.com>
2018-06-04 14:26:36 +0200

    Add files via upload

--- src/main/java/jpa/Student.java
+++ src/main/java/jpa/Student.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jpa;
 import javax.persistence.*;
 import java.util.Date;
@@ -6,7 +5,9 @@ import java.util.Date;
 
 @Entity
 @NamedQueries({
+        //@NamedQuery(name="selectAllS",query="SELECT p FROM Student p"),
         @NamedQuery(name="selectPNumberS",query="SELECT t FROM Student t WHERE LOCATE(:filt,t.pnumber) >0 ")
+
 })
 public class Student {
     @Id
@@ -22,7 +23,7 @@ public class Student {
     private String city;
 
     @OneToOne (mappedBy = "student")
-    private Users users;
+    private Login login;
 
 
     public Student() {
@@ -37,7 +38,7 @@ public class Student {
         this.adress = adress;
         this.postzip = postzip;
         this.city = city;
-        this.users = users;
+        this.login = login;
     }
 
     public Long getId() {
@@ -113,140 +114,3 @@ public class Student {
         this.city = city;
     }
 }
-=======
-package jpa;
-import javax.persistence.*;
-
-
-@Entity
-public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String pnumber;
-    private String email;
-    private String firstname;
-    private String lastname;
-    private String password;
-    private String dateOfBirth;
-    private String telNumber;
-    private String adress;
-    private String postzip;
-    private String city;
-    private String country;
-
-
-    public Student() {
-    }
-
-    public Student(String pnumber, String email, String firstname, String lastname, String password, String dateOfBirth, String telNumber, String adress, String postzip, String city, String country) {
-        this.pnumber = pnumber;
-        this.email = email;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.password = password;
-        this.dateOfBirth = dateOfBirth;
-        this.telNumber = telNumber;
-        this.adress = adress;
-        this.postzip = postzip;
-        this.city = city;
-        this.country = country;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPnumber() {
-        return pnumber;
-    }
-
-    public void setPnumber(String pnumber) {
-        this.pnumber = pnumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getTelNumber() {
-        return telNumber;
-    }
-
-    public void setTelNumber(String telNumber) {
-        this.telNumber = telNumber;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    public String getPostzip() {
-        return postzip;
-    }
-
-    public void setPostzip(String postzip) {
-        this.postzip = postzip;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-}
->>>>>>> 76a7c005a05286c394d6c6648f0aedff7fd1c139
